074e1580d12563b64f2280da7d3211cb7d397f74
haneeshm <150122523+sagarmech98@users.noreply.github.com>
2025-05-31 01:15:35 +0530

    Refactor module definitions for databases and applications in main.tf and variables.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 
-module "frontend" {
+module "databases" {
+  for_each                  = var.databases
   source                     = "./component"
-  name                       = "frontend"
+  name                       = each.key
   location                   = var.location
   rg_name                    = var.rg_name
   storage_image_reference_id = var.storage_image_reference_id
@@ -10,98 +11,11 @@ module "frontend" {
   network_security_group_id  = var.network_security_group_id
 }
 
-
-module "mongodb" {
+module "applications" {
+  depends_on                 = [ module.databases ]
+  for_each                   = var.databases
   source                     = "./component"
-  name                       = "mongodb"
-  location                   = var.location
-  rg_name                    = var.rg_name
-  storage_image_reference_id = var.storage_image_reference_id
-  ip_configuration_subnet_id = var.ip_configuration_subnet_id
-  zone_name                  = var.zone_name
-  network_security_group_id  = var.network_security_group_id
-}
-
-module "catalogue" {
-  source                     = "./component"
-  name                       = "catalogue"
-  location                   = var.location
-  rg_name                    = var.rg_name
-  storage_image_reference_id = var.storage_image_reference_id
-  ip_configuration_subnet_id = var.ip_configuration_subnet_id
-  zone_name                  = var.zone_name
-  network_security_group_id  = var.network_security_group_id
-}
-
-module "user" {
-  source                     = "./component"
-  name                       = "user"
-  location                   = var.location
-  rg_name                    = var.rg_name
-  storage_image_reference_id = var.storage_image_reference_id
-  ip_configuration_subnet_id = var.ip_configuration_subnet_id
-  zone_name                  = var.zone_name
-  network_security_group_id  = var.network_security_group_id
-}
-
-module "redis" {
-  source                     = "./component"
-  name                       = "redis"
-  location                   = var.location
-  rg_name                    = var.rg_name
-  storage_image_reference_id = var.storage_image_reference_id
-  ip_configuration_subnet_id = var.ip_configuration_subnet_id
-  zone_name                  = var.zone_name
-  network_security_group_id  = var.network_security_group_id
-}
-
-module "cart" {
-  source                     = "./component"
-  name                       = "cart"
-  location                   = var.location
-  rg_name                    = var.rg_name
-  storage_image_reference_id = var.storage_image_reference_id
-  ip_configuration_subnet_id = var.ip_configuration_subnet_id
-  zone_name                  = var.zone_name
-  network_security_group_id  = var.network_security_group_id
-}
-
-module "mysql" {
-  source                     = "./component"
-  name                       = "mysql"
-  location                   = var.location
-  rg_name                    = var.rg_name
-  storage_image_reference_id = var.storage_image_reference_id
-  ip_configuration_subnet_id = var.ip_configuration_subnet_id
-  zone_name                  = var.zone_name
-  network_security_group_id  = var.network_security_group_id
-}
-
-module "shipping" {
-  source                     = "./component"
-  name                       = "shipping"
-  location                   = var.location
-  rg_name                    = var.rg_name
-  storage_image_reference_id = var.storage_image_reference_id
-  ip_configuration_subnet_id = var.ip_configuration_subnet_id
-  zone_name                  = var.zone_name
-  network_security_group_id  = var.network_security_group_id
-}
-
-module "rabbitmq" {
-  source                     = "./component"
-  name                       = "rabbitmq"
-  location                   = var.location
-  rg_name                    = var.rg_name
-  storage_image_reference_id = var.storage_image_reference_id
-  ip_configuration_subnet_id = var.ip_configuration_subnet_id
-  zone_name                  = var.zone_name
-  network_security_group_id  = var.network_security_group_id
-}
-
-module "payment" {
-  source                     = "./component"
-  name                       = "payment"
+  name                       = each.key
   location                   = var.location
   rg_name                    = var.rg_name
   storage_image_reference_id = var.storage_image_reference_id
--- variables.tf
+++ variables.tf
@@ -24,3 +24,23 @@ variable "network_security_group_id" {
   default = "/subscriptions/72129bc2-1be7-4c6e-971e-9375ebd6c232/resourceGroups/Project/providers/Microsoft.Network/networkSecurityGroups/allow-all"
 }
 
+variable "databases" {
+  default = {
+    mongodb = {}
+    rabbitmq = {}
+    mysql = {}
+    redis = {}
+  }
+}
+
+
+variable "applications" {
+  default = {
+    catalogue = {}
+    user = {}
+    cart = {}
+    payment = {}
+    shipping = {}
+    frontend = {}
+  }
+}
