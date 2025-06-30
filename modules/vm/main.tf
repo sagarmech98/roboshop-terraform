fde42a25500a72a50935d6284681ac3a0e2f2a06
haneeshm <150122523+sagarmech98@users.noreply.github.com>
2025-06-30 18:59:15 +0530

    Update Ansible installation command to include hvac and modify ansible-pull command to use token variable

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -68,7 +68,7 @@ resource "null_resource" "anisble" {
   provisioner "remote-exec" {
     inline = [
         "sudo dnf install python3.12 python3.12-pip -y",
-        "sudo pip3.12 install ansible",
+        "sudo pip3.12 install ansible hvac",
         "ansible-pull -i localhost, -U https://github.com/sagarmech98/roboshop-ansible roboshop.yaml -e app_name=${var.name} -e env=dev -e token=${var.token}"  
     ]
     }
